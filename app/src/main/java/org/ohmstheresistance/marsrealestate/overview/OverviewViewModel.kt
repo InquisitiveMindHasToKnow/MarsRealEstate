29b21a6871d38757109bac23ea86c5ab7f19dbb7
InquisitiveMindHasToKnow <omarraymond@pursuit.org>
2020-03-22 21:11:06 -0400

    Used moshi to convert the json objects to kotlin objects and currently displaying the total amount of properties in the overview fragment through the OverviewViewModel.

--- app/src/main/java/org/ohmstheresistance/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/org/ohmstheresistance/marsrealestate/overview/OverviewViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import org.ohmstheresistance.marsrealestate.network.MarsApi
+import org.ohmstheresistance.marsrealestate.network.MarsProperty
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -21,13 +22,16 @@ class OverviewViewModel : ViewModel() {
 
     private fun getMarsRealEstateProperties() {
 
-        MarsApi.retrofitService.getProperties().enqueue( object: Callback<String> {
-            override fun onFailure(call: Call<String>, t: Throwable) {
+        MarsApi.retrofitService.getProperties().enqueue( object : Callback<List<MarsProperty>> {
+
+            override fun onFailure(call: Call<List<MarsProperty>>, t: Throwable) {
                 _response.value = "Failure: " + t.message
             }
 
-            override fun onResponse(call: Call<String>, response: Response<String>) {
-                _response.value = response.body()
+            override fun onResponse(call: Call<List<MarsProperty>>, response: Response<List<MarsProperty>>) {
+
+                _response.value = "Success: ${response.body()?.size} Mars properties received!"
+
             }
         })
     }
