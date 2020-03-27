d977c29c12e864667d4274aca2bebc41b682f772
InquisitiveMindHasToKnow <omarraymond@pursuit.org>
2020-03-27 19:29:16 -0400

    Set androidExtention experimental to true to enable @Parcelize call so android keeps track of parcelables instead of having to manually do it.

--- app/src/main/java/org/ohmstheresistance/marsrealestate/network/MarsProperty.kt
+++ app/src/main/java/org/ohmstheresistance/marsrealestate/network/MarsProperty.kt
@@ -1,9 +1,12 @@
 package org.ohmstheresistance.marsrealestate.network
 
+import android.os.Parcelable
 import com.squareup.moshi.Json
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class MarsProperty(
     val id: String,
     @Json(name = "img_src") val img_srcUrl: String,
     val type: String,
-    val price: Double)
+    val price: Double): Parcelable
